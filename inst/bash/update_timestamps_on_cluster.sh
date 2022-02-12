ba7d7984af2d3e4de224f4db0c620b05b9f2e497
chase mateusiak <chase.mateusiak@gmail.com>
2022-02-12 16:45:07 -0600

    adding cron job script to inst

---
+++ inst/bash/update_timestamps_on_cluster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# place this in your $HOME and follow these directions to set up a cron
+# job
+# https://help.ubuntu.com/community/CronHowto
+
+export PATH=/opt/slurm/bin:/opt/slurm/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+srun --chdir /scratch/mblab/$USER find /scratch/mblab/$USER -exec touch {} +
